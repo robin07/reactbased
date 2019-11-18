39d25594eae858642987c0fcc473eab8fb30c154
robin07 <robbiesingh.singh84@gmail.com>
2019-11-18 10:25:53 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar >
+        <div className="container">
+          <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
     </div>
   );
+}
 }
 
 export default App;
